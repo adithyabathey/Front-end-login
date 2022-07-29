883a196b23715ee44850cb20e8b39ca85963b626
adithyabathey <85573928+adithyabathey@users.noreply.github.com>
2022-07-29 16:21:59 +0530

    register page done

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -7,21 +8,52 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  firstName: any;
-  pass: any;
-  constructor(private router: Router) { }
+  Name: any;
+  email: any;
+  pass:any;
+  employees: any;
+  help:any;
+  constructor(private router: Router,private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get('http://localhost:3000/getemployees').subscribe((result:any)=>{
+      console.log(result);
+      this.employees = result;
+      // this.username = result
+    //   for(let i=0; i<result.length; i++){
+    //     JSON.stringify(result[i].Name);
+    //     console.log(result[i].Name);
+    //     this.user.push(result[i].Name);
+    //   }
+    })
   }
   createaccount(){
-    let user=this.firstName;
-    let password=this.pass;
-    console.log(this.firstName);
+    let user=this.Name;
+    // let password=this.pass;
+    console.log(this.Name);
+    console.log(this.email);
+    console.log(this.pass);
+    let record = {
+      Name: this.Name,
+      Password: this.pass
+    }
+    this.help=0;
+      if(this.Name==undefined && this.pass==undefined){
+        alert("fill out the name and password");
+        this.help=1; 
+    }
+    console.log(record);
+    this.http.post('http://localhost:3000/post',record).subscribe(res=>{
+      if(!this.help){
+        alert("Create Succesfully");
+        console.log(res);
+        this.router.navigate(['login']);
+      }
+    })
     // user.push_back(user);
     
     // let work=0;
     // if()
-    this.router.navigate(['login']);
   }
   back(){
     this.router.navigate(['login']);
